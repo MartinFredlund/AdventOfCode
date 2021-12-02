a34813bfb58359e44c93505cee44e76eeac808c5
Martin Fredlund <ma1233fr-s@student.lu.se>
2021-12-02 21:32:03 +0100

    First and sec day done in python

---
+++ Day1/SonarSweepWind.py
@@ -0,0 +1,19 @@
+def nextValue(value, line):    
+    for x in range(3):
+        value[x] = value[x+1]
+    value[3] = int (line)
+    return value
+    
+file = open('input.txt', 'r')
+value = [0,0,0,0]
+amount = 0
+
+for x in range(1, 3):
+    value[x] = int (file.readline())
+
+for line in file:
+    value = nextValue(value, line)
+    if(value[0] + value[1] + value[2] < value[1] + value[2] + value[3]):
+        amount += 1
+print(amount)
+file.close()
