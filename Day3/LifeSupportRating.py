fa35dfe88557206ae6ec2b53facb5f3313b03e25
Martin Fredlund <ma1233fr-s@student.lu.se>
2021-12-03 23:11:03 +0100

    solved day3 in python

---
+++ Day3/LifeSupportRating.py
@@ -0,0 +1,43 @@
+def value(input, mostCommon):
+    pos = 1
+    listOne.clear()
+    listZero.clear()
+    while(len(input) != 1):
+        for line in input:
+            if(line[pos] == "1"):
+                listOne.append(line)
+            else:
+                listZero.append(line)
+        input.clear()
+        if(mostCommon):
+            if(len(listOne) >= len(listZero)):
+                input.extend(listOne)
+            else:
+                input.extend(listZero)
+        else:
+            if(len(listOne) < len(listZero)):
+                input.extend(listOne)
+            else:
+                input.extend(listZero)
+        listOne.clear()
+        listZero.clear()
+        pos += 1
+    return int(input[0], 2)
+        
+file = open('input.txt', 'r')
+listOne = []
+listZero = []
+for line in file:
+    if(line[0] == "1"):
+        listOne.append(line.rstrip("\n"))
+    else:
+        listZero.append(line.rstrip("\n"))
+oxygen = []
+co2 = []
+if(len(listOne) > len(listZero)):
+    oxygen.extend(listOne)
+    co2.extend(listZero)
+else:
+    oxygen.extend(listZero)
+    co2.extend(listOne)
+print(value(oxygen, True) * value(co2, False))
