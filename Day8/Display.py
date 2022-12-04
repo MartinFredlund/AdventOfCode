564d02c58071f14dc887b2d1cd6c498a12468d36
Martin Fredlund <ma1233fr-s@student.lu.se>
2022-12-04 22:19:51 +0100

    git structure

--- Day8/Display.py
+++
@@ -1,9 +0,0 @@
-file = open("input.txt", "r")
-amount = 0
-for line in file:
-    nextLine = line.split(" | ")
-    seqs = nextLine[1].split()
-    for seq in seqs:
-        if(len(seq) in {2,3,4,7}):
-            amount += 1
-print(amount)
