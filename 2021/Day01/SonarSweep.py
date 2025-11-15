a2eaca095181741ce5e36e286bf1158a54fb1faf
MartinFredlund <35783962+MartinFredlund@users.noreply.github.com>
2025-11-15 23:30:55 +0100

    Delete 2021/Day01 directory

--- 2021/Day01/SonarSweep.py
+++
@@ -1,9 +0,0 @@
-lines = open('input.txt', 'r')
-lastLine = int (lines.readline())
-amount = 0
-for line in lines:
-    if int (line) > lastLine:
-        amount += 1
-    lastLine = int (line)
-print(amount)
-lines.close()
