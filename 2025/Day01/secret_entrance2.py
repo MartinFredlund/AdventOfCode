543443af8145ea88fc945591c103ab6e37b33a65
Martin <martin.fredlund@lime.tech>
2025-12-01 16:09:22 +0100

    Part 2

--- 2025/Day01/secret_entrance2.py
+++ 2025/Day01/secret_entrance2.py
@@ -1,31 +1,25 @@
-file = open("input.txt", "r")
-zeros = 0
+file =  open("input.txt")
+
 current_value = 50
+score = 0
 
-for turn in file:
-    direction = turn[0]
-    amount = int(turn[1:])
-    if direction == "L":
-        current_value -= amount
+for row in file:
+    if row[0] == "L":
+        step = int(row[1:]) * -1
     else:
-        current_value += amount
-    print("---------------------------------")
-    print("New row: Current " + str(current_value) + " row: " + turn)
-    
-    #Count how many times we pass through 0
-    if current_value < 0:
-        zeros += abs(current_value) // 100 + 1
-    elif current_value > 99:
-        zeros += current_value // 100
-    elif current_value == 0:
-        zeros += 1
-    
-    #Wrap to range 0-99
-    current_value = current_value % 100
-    
-    print("After wrap: New value : " + str(current_value) + " zeros " + str(zeros))
+        step = int(row[1:])
+    prev = current_value
+    div, current_value = divmod(current_value + step, 100)
 
-
+    # Count boundary crossings
+    crossings = abs(div)
+    # Don't count if we start at 0 and move left (negative div)
+    if prev == 0 and div < 0:
+        crossings -= 1
+    # Add 1 if we land on 0 while moving left
+    if current_value == 0 and step < 0:
+        crossings += 1
     
-
+    score += crossings
     
+print(score)
